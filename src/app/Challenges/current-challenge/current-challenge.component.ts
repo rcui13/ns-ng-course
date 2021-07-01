28c29041cd82bce27291e395bc48b03575e2e5bd
Ianyliu <ian.liu@g.northernacademy.org>
2021-07-01 17:43:19 -0400

    test

---
+++ src/app/Challenges/current-challenge/current-challenge.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core"
+@Component({
+  selector: 'ns-current-challenge',
+  templateUrl: './current-challenge.component.html',
+  styleUrls: ['.current-challenge.component.css'],
+  moduleId: module.id
+})
+export class CurrentChallengeComponent {}
